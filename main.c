d182a08b53f8d32b9f3073d698a3ada4909394f0
ENG-Saad-Mahmoud <saadmah65@gmail.com>
2024-10-25 20:33:19 +0300

    three different integers

--- main.c
+++ main.c
@@ -2,13 +2,29 @@
 #include <stdlib.h>
 
 int main()
-{float a,b,c;
-
-    printf("Enter any three Angels:  ");
-    scanf("%f%f%f",&a,&b,&c);
-    if(a+b+c==180){
-        printf("triangle Validity: ");}
-     else{
-     printf("Wrong!!!!!!!!!!....\n Enter the Right Angles ");}
+{
+   int a,b,c;
+   printf("enter three integer :\n ");
+   scanf("%d%d%d",&a,&b,&c);
+   printf("the sum is = %d\n",a+b+c);
+   printf("the product is = %d\n",a*b*c);
+   printf("the average is = %d\n",(a+b+c)/3);
+   if(a>b&&a>c){
+    printf("the largest is = %d\n",a);
+   }else if(b>c&&b>a){
+   printf("the largest is = %d\n",b);
+   }
+   else if(b<c&&c>a){
+   printf("the largest is = %d\n",c);
+   }
+   if(a<b&&a<c){
+    printf("the smallest is = %d\n",a);
+   }else if(a>b&&c>b){
+    printf("the smallest is = %d\n",b);
+   } else if(a>c&&c<b){
+    printf("the smallest is = %d\n",c);
+   }
+   else
+    printf("these number are Equal");
     return 0;
 }
