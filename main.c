558d61a2760ead525df43cecd0c8b859147abb8d
ENG-Saad-Mahmoud <saadmah65@gmail.com>
2024-10-25 20:31:08 +0300

    Triangle Validity

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{float a,b,c;
+
+    printf("Enter any three Angels:  ");
+    scanf("%f%f%f",&a,&b,&c);
+    if(a+b+c==180){
+        printf("triangle Validity: ");}
+     else{
+     printf("Wrong!!!!!!!!!!....\n Enter the Right Angles ");}
+    return 0;
+}
